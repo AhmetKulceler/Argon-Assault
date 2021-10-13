b5818929c1e075e589e7b0748f3c4c2e7c6fc505
AhmetKulceler <ahmetkulceler@outlook.com>
2021-10-13 19:58:32 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float controlSpeed = 25.0f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 6f;
+
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -10f;
+
+    [SerializeField] float positionYawFactor = 2f;
+
+    [SerializeField] float controlRollFactor = -15f;
+
+    float xControl, yControl;
+
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    void ProcessTranslation()
+    {
+        xControl = Input.GetAxis("Horizontal");
+        yControl = Input.GetAxis("Vertical");
+
+        float xOffset = xControl * controlSpeed * Time.deltaTime;
+        float yOffset = yControl * controlSpeed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+
+        float limitedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float limitedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(limitedXPos, limitedYPos, transform.localPosition.z);
+    }
+
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControl = yControl * controlPitchFactor;
+
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+
+        float rollDueToControl = xControl * controlRollFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToControl;
+        float yaw = yawDueToPosition;
+        float roll = rollDueToControl;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+}
