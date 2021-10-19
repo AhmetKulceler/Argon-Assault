d6c0f95906334713f80a1c454f5011fc5559de35
AhmetKulceler <ahmetkulceler@outlook.com>
2021-10-19 19:21:30 +0300

    Basic UI and Scoring

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,38 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject enemyExplosion;
+    [SerializeField] GameObject enemyHit;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit;
+    [SerializeField] int hitPoints;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
 
     private void OnParticleCollision(GameObject other)
+    {
+        ProcessHitAndScore();
+        
+        if (hitPoints <= 0)
+        {
+            DestroyEnemy();
+        }
+    }
+
+    private void ProcessHitAndScore()
+    {
+        hitPoints -= scorePerHit;
+        GameObject vfx = Instantiate(enemyHit, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    private void DestroyEnemy()
     {
         GameObject vfx = Instantiate(enemyExplosion, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
